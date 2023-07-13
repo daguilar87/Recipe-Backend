690f3173da1a9291f1bec01792c0f4140452366d
Daniel Aguilar <daguilar8749@gmail.com>
2023-07-12 21:00:29 -0700

    first commit

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from app import app
+
+from flask import render_template
+
+@app.route('/')
+def homePage():
+    return render_template('index.html')
+
+@app.route('/login')
+def loginPage():
+    return render_template('login.html')
